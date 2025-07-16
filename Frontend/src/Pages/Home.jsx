7017d55a32e53535cff554cad386715a792b8361
Siddharth Potphode <siddharthpotphode7@gmail.com>
2025-07-16 21:51:47 +0530

    again

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="w-[70vw] ml-[3vw] flex justify-center">
+        <div className="w-[70vw] ml-[1vw] flex justify-center">
           <LastFoot />
         </div>
       </main>
